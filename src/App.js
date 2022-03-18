9aa715f128106726cb9c2ed85d7f52c58aa5abd9
srnq <77878502+xzjenny@users.noreply.github.com>
2022-03-17 23:55:32 -0700

    Created Firebase prototype

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { auth } from "./firebase-config"
+import { signOut } from 'firebase/auth';
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 
 function App() {
+  
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/";
+    });
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        {!isAuth ? (
+          <div></div>
+        ) : (
+          <>
+            <Link to="/home"> Home </Link>
+            <Link to="/profile"> My Profile </Link>
+            <button onClick={signUserOut}> Log Out </button>
+          </>
+        )}
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Login setIsAuth={setIsAuth}/>} />
+        <Route path="/profile" element={<Profile isAuth={isAuth}/>} />
+        <Route path="/home" element={<Home isAuth={isAuth}/>} />
+      </Routes>
+    </Router>
   );
 }
 
