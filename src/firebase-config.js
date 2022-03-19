541a69c54cc8c1faa69ed5e5a76c5e401a1b802c
srnq <77878502+xzjenny@users.noreply.github.com>
2022-03-19 14:02:32 -0700

    Made it so all users show up on Home except current user

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,23 +3,24 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCGcng2hn6QpYRUrVFYKy4OhlymqCVrQew",
-//   authDomain: "study-buddies-61e13.firebaseapp.com",
-//   projectId: "study-buddies-61e13",
-//   storageBucket: "study-buddies-61e13.appspot.com",
-//   messagingSenderId: "857954946818",
-//   appId: "1:857954946818:web:2baac59bfae2214157bc1b"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAT2AJuwe3RCnNmbotwcQWrh3GJVUr4alc",
-  authDomain: "test2-a4ea9.firebaseapp.com",
-  projectId: "test2-a4ea9",
-  storageBucket: "test2-a4ea9.appspot.com",
-  messagingSenderId: "905989965054",
-  appId: "1:905989965054:web:7f21531cbc85b75c18d1af"
+  apiKey: "AIzaSyCGcng2hn6QpYRUrVFYKy4OhlymqCVrQew",
+  authDomain: "study-buddies-61e13.firebaseapp.com",
+  projectId: "study-buddies-61e13",
+  storageBucket: "study-buddies-61e13.appspot.com",
+  messagingSenderId: "857954946818",
+  appId: "1:857954946818:web:2baac59bfae2214157bc1b"
 };
+
+// Test2
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAT2AJuwe3RCnNmbotwcQWrh3GJVUr4alc",
+//   authDomain: "test2-a4ea9.firebaseapp.com",
+//   projectId: "test2-a4ea9",
+//   storageBucket: "test2-a4ea9.appspot.com",
+//   messagingSenderId: "905989965054",
+//   appId: "1:905989965054:web:7f21531cbc85b75c18d1af"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
