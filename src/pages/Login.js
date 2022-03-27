b1d88818395ac59eba703f3ade4ae705b62934b8
GilenFC <gwuhou@ucsd.edu>
2022-03-26 20:51:23 -0700

    fixed noticebox alignment and added background

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import {auth, provider} from '../firebase-config';
 import {signInWithPopup } from 'firebase/auth';
 import { useNavigate } from "react-router-dom";
 import './Login.css';
+import logo from '../images/group-of-happy-business-people.svg';
+
 
 function Login({ setIsAuth }) {
   
@@ -17,20 +19,37 @@ function Login({ setIsAuth }) {
   };
 
   return (
+    
     <div className="loginPage">
       <div>
-        <iframe id="topbar"></iframe>
+        <div id="topbar"></div>
         <h1 id="StudyBuddies">Study Buddies</h1>
         <h2 className="center" id="tagline">Insert some tagline or something.</h2>
+        {/* <div className='illustration'>
+          <img src={logo}alt='illustration' height={1000}/>
+        </div> */}
+
+        <figure>
+          <div>
+            <div className='illustration'/>
+          </div>
+        </figure>
+
+
       </div>
-      <button id="login" onClick={signInWithGoogle}>Sign in with Google</button>
-      <iframe id="noticebox"></iframe>
-        <h2 id="notice">NOTICE</h2>
-        <p id="disclaimer">On this site, your school email address is <b>PUBLICLY AVAILABLE </b>
-        to other users. By signing up, you agree to have your school email address 
-        displayed on your profile.<br></br>
-        When working with your study buddy, please be mindful of Academic Integrity 
-        rules specified by the univsersity.</p>
+      
+        <button id="login" onClick={signInWithGoogle}>Sign in with Google</button>
+      
+        <div className='noticebox'>
+          <div className='content'>
+            <h2 id="notice">NOTICE</h2>
+            <p id="disclaimer">On this site, your school email address is <b>PUBLICLY AVAILABLE </b>
+            to other users. By signing up, you agree to have your school email address 
+            displayed on your profile.<br></br>
+            When working with your study buddy, please be mindful of Academic Integrity 
+            rules specified by the univsersity.</p>
+          </div>
+        </div>
     </div>
   )
 }
